02b93ed155868925979f9dd9f6a5289cbfbb20ab
woosang <woosang3078@hanmail.net>
2023-04-11 08:51:16 +0900

    docs: 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Introduce from './introduce';
 
-function App() {
+const App: React.FC = () => {
   return (
-    
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Introduce />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
