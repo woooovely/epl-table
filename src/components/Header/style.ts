c6e739a78fa1a7c2653f8caffd73e58f77c2d8c9
woosang <woosang3078@hanmail.net>
2023-04-11 17:21:53 +0900

    스타일 파일 추가

---
+++ src/components/Header/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.nav`
+  width: 100%;
+  height: 67px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`
+
+export const LogoText = styled.p`
+  font-family: 'Premier League';
+  font-size: 25px;
+`
+
+export const CategoryContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-evenly;
+  width: 521px;
+`
+
+export const Categories = styled.li`
+  font-size: 15px;
+`
