c3f224bf48d5ebe7f30bc3f9093afe4e71320efd
woosang <woosang3078@hanmail.net>
2023-04-11 10:11:38 +0900

    feat: 클릭 이벤트 추가

--- src/introduce/index.tsx
+++ src/introduce/index.tsx
@@ -1,10 +1,13 @@
 import * as S from "./style";
 import logo from "../assets/svgs/premier_league.svg";
 import useTitle from '../hook/title';
+import { useNavigate } from 'react-router-dom';
 
 const Introduce = () => {
     const titleUpdater = useTitle('불러오는 중...');
     setTimeout(() => titleUpdater('EPL Table - 잉글랜드 프리미어리그 조회 서비스'))
+
+    const navigate = useNavigate();
 
   return (
     <S.Container>
@@ -23,7 +26,7 @@ const Introduce = () => {
             <img src={logo} alt="프리미어리그 로고" />
           </S.Right>
         </S.Section>
-        <S.Button>이동하기</S.Button>
+        <S.Button onClick={() => navigate('/rank')}>이동하기</S.Button>
       </S.IntroduceContainer>
     </S.Container>
   );
