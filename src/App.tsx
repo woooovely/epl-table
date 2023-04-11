e19eeca7903b8f7d4f92f64783d2715c6c46985e
woosang <woosang3078@hanmail.net>
2023-04-11 17:22:15 +0900

    docs: 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Introduce from './introduce';
+import TeamRank from './rank';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Introduce />}></Route>
+        <Route path="/rank" element={<TeamRank />}></Route>
       </Routes>
     </BrowserRouter>
   )
