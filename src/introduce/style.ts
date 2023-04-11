3ef278c29b2ff876ed936c12abb653cfbde45be1
woosang <woosang3078@hanmail.net>
2023-04-11 09:41:22 +0900

    스타일 파일 추가

---
+++ src/introduce/style.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const IntroduceContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Section = styled.div`
+  width: 1200px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TextContainer = styled.div``
+
+export const Text = styled.p`
+  font-size: 45px;
+  font-weight: 600;
+`;
+
+export const Right = styled.div`
+  width: 600px;
+  height: 400px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Button = styled.button`
+  width: 170px;
+  height: 60px;
+  border: none;
+  border-radius: 20px;
+  font-size: 18px;
+  background-color: #FB9464;
+  color: white;
+  font-weight: 600;
+  position: fixed;
+  bottom: 20px;
+  opacity: 0;
+`
