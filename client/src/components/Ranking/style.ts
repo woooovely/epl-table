ac15014819581c36341be56db81eaae810c8fa8c
woosang <woosang3078@hanmail.net>
2023-04-12 15:41:31 +0900

    스타일 파일 추가

---
+++ client/src/components/Ranking/style.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  margin-top: 50px;
+  width: 500px;
+  border: 1px solid #D6D6D6;
+`;
+
+export const Head = styled.thead``;
+
+export const TableTr = styled.tr``;
+
+export const TableTh = styled.th`
+  margin-right: 50px;
+`;
+
+export const Body = styled.tbody``;
+
+export const ListTr = styled.tr``;
+
+export const ListTd = styled.td`
+  text-align: center;
+  :nth-child(2) {
+    display: flex;
+    align-items: center;
+    margin-left: 45px;
+    margin-top: 15px;
+    padding-left: 10px;
+  }
+`;
+
+export const TeamLogo = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+
+export const TeamName = styled.b`
+  margin-left: 10px;
+`
