692ef49966dbd91b34603bfad3dbcaa0c97697c8
woosang <woosang3078@hanmail.net>
2023-04-12 11:28:54 +0900

    docs: API 토큰 숨김

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ app.get('/premierleague', async (req, res) => {
     try {
         const response = await axios.get('https://api.football-data.org/v4/competitions/PL/standings', {
             headers: {
-                'X-Auth-Token': 'e626339ce3c945e68bc7e7691ac3b077'
+                'X-Auth-Token': process.env.API_TOKEN
             }
         });
         res.send(response.data);
