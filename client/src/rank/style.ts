82df2b4057d60cc39a94b53156fae7ea81a80d3e
woosang <woosang3078@hanmail.net>
2023-04-12 11:30:00 +0900

    스타일 파일 추가

---
+++ client/src/rank/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const BannerImage = styled.img`
+  width: 100%;
+`
+
+export const Text = styled.p`
+  font-size: 35px;
+  font-weight: 600;
+  color: #21109E;
+  text-align: center;
+  margin-top: 50px;
+`
+
+export const Stick = styled.div`
+  width: 230px;
+  height: 1px;
+  background-color: #C4C4C4;
+  margin: 0 auto;
+`
+
+export const TableContainer = styled.div`
+  width: 400px;
+  margin: 0 auto;
+`
