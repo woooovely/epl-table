d6dc2bebc9ad42eb2ec04f78480c0d8b74f8ed28
woosang <woosang3078@hanmail.net>
2023-04-13 10:43:25 +0900

    스타일 파일 추가

---
+++ client/src/pages/scorer/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Banner = styled.img`
+  width: 100%;
+`
+
+export const Text = styled.p`
+  font-size: 25px;
+  font-weight: 600;
+  text-align: center;
+`
+
+export const Stick = styled.div`
+  width: 190px;
+  height: 1px;
+  background-color: #C4C4C4;
+  margin: 0 auto;
+`
