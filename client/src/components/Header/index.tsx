eb58368cfa478884ffb887013be8e1ccbf5f0cad
woosang <woosang3078@hanmail.net>
2023-04-13 10:43:52 +0900

    docs: 헤더 메뉴 한국어 변경

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -7,13 +7,13 @@ const Header = () => {
             <S.LogoText>EPL Table</S.LogoText>
             <S.CategoryContainer>
                 <S.Categories>
-                  <S.LinkStyle to="/">About</S.LinkStyle>
+                  <S.LinkStyle to="/">홈</S.LinkStyle>
                 </S.Categories>
                 <S.Categories>
-                  <S.LinkStyle to="/rank">Ranking</S.LinkStyle>
+                  <S.LinkStyle to="/rank">팀 순위</S.LinkStyle>
                 </S.Categories>
                 <S.Categories>
-                  <S.LinkStyle to="/top-scorer">Top Scorer</S.LinkStyle>
+                  <S.LinkStyle to="/top-scorer">득점왕 순위</S.LinkStyle>
                 </S.Categories>
             </S.CategoryContainer>
           </S.HeaderContainer>
