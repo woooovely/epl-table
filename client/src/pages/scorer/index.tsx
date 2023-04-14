681765ecdbce98c9838c13617fd513a74415f17b
woosang <woosang3078@hanmail.net>
2023-04-14 17:10:20 +0900

    design: 정렬 위치 수정

--- client/src/pages/scorer/index.tsx
+++ client/src/pages/scorer/index.tsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header';
 import * as S from './style';
 import banner from '../../assets/svgs/banner.svg';
+import ScorerTable from '../../components/Scorer';
 
 const Scorer = () => {
     return (
@@ -9,6 +10,9 @@ const Scorer = () => {
             <S.Banner src={banner} alt="배너" />
             <S.Text>프리미어리그 득점 순위</S.Text>
             <S.Stick />
+            <S.TableContainer>
+                <ScorerTable />
+            </S.TableContainer>
         </div>
     )
 }
