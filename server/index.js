6c4a09f643c1711b6ca7c22c63068ad4cb413438
woosang <woosang3078@hanmail.net>
2023-04-14 17:07:36 +0900

    feat: 득점왕 API 추가

--- server/index.js
+++ server/index.js
@@ -1,37 +1,85 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
-app.get('/premierleague', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get("/premierleague", (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const response = yield axios_1.default.get('https://api.football-data.org/v4/competitions/PL/standings', {
-            headers: {
-                'X-Auth-Token': 'e626339ce3c945e68bc7e7691ac3b077'
-            }
-        });
-        res.send(response.data);
+      const response = yield axios_1.default.get(
+        "https://api.football-data.org/v4/competitions/PL/standings",
+        {
+          headers: {
+            "X-Auth-Token": "e626339ce3c945e68bc7e7691ac3b077",
+          },
+        }
+      );
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Interval Server Error");
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).send('Interval Server Error');
+  })
+);
+app.get("/premierleague/scorers", (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    try {
+      const response = yield axios_1.default.get(
+        "https://api.football-data.org/v4/competitions/PL/scorers",
+        {
+          headers: {
+            "X-Auth-Token": "e626339ce3c945e68bc7e7691ac3b077",
+          },
+        }
+      );
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Interval Server Error");
     }
-}));
+  })
+);
 const PORT = 4000;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,54 @@
-import express from 'express';
-import axios from 'axios';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import axios from "axios";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
 
+app.use(cors(corsOptions));
 
-app.get('/premierleague', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.football-data.org/v4/competitions/PL/standings', {
-            headers: {
-                'X-Auth-Token': process.env.API_TOKEN
-            }
-        });
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Interval Server Error')
-    }
+app.get("/test", (req: Request, res: Response) => {
+  console.log("abcd");
+});
+
+app.get("/premierleague/scorers", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(
+      "https://api.football-data.org/v4/competitions/PL/scorers",
+      {
+        headers: {
+          "X-Auth-Token": process.env.API_TOKEN,
+        },
+      }
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/premierleague", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(
+      "https://api.football-data.org/v4/competitions/PL/standings",
+      {
+        headers: {
+          "X-Auth-Token": process.env.API_TOKEN,
+        },
+      }
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 const PORT = 4000;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
