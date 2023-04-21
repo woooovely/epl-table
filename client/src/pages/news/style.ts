45b6ec61dec1c3a293a9d31c18d19d4ad3e55900
woosang <woosang3078@hanmail.net>
2023-04-21 11:47:50 +0900

    스타일 파일 추가

---
+++ client/src/pages/news/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const BannerImage = styled.img`
+  width: 100%;
+`
+
+export const NewsContainer = styled.div`
+  display: flex;
+  width: 1200px;
+`
