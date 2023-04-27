32aaf1c491b841c8ee08b9e7727da8cca437cd43
woosang <woosang3078@hanmail.net>
2023-04-27 15:29:21 +0900

    스타일 파일 추가

---
+++ client/src/components/News/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  font-size: 30px;
+  font-weight: 550;
+  font-family: 'Pretendard';
+`
+
+export const NewsItemBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Thumbnail = styled.div`
+  margin-right: 1rem;
+`
+
+export const Image = styled.img`
+  display: block;
+  width: 160px;
+  height: 100px;
+  object-fit: cover;
+`
