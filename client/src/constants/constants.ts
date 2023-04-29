99ac8e303f710ca4b2d22aced7c98ea82c241583
woosang <woosang3078@hanmail.net>
2023-04-29 21:47:36 +0900

    feat: 한국어 번역 추가

---
+++ client/src/constants/constants.ts
@@ -0,0 +1,37 @@
+export const teamNameMap: { [key: string]: string } = {
+  "Arsenal FC": "아스날",
+  "Manchester City FC": "맨체스터 시티",
+  "Newcastle United FC": "뉴캐슬 유나이티드",
+  "Manchester United FC": "맨체스터 유나이티드",
+  "Tottenham Hotspur FC": "토트넘 홋스퍼",
+  "Aston Villa FC": "아스톤 빌라",
+  "Brighton & Hove Albion FC": "브라이튼 호브 알비온",
+  "Liverpool FC": "리버풀",
+  "Brentford FC": "브렌트포드",
+  "Fulham FC": "풀럼",
+  "Chelsea FC": "첼시",
+  "Crystal Palace FC": "크리스탈 팰리스",
+  "Wolverhampton Wanderers FC": "울버햄튼 원더러스",
+  "West Ham United FC": "웨스트햄",
+  "AFC Bournemouth": "본머스",
+  "Leeds United FC": "리즈",
+  "Everton FC": "에버튼",
+  "Nottingham Forest FC": "노팅엄",
+  "Leicester City FC": "레스터",
+  "Southampton FC": "사우스햄튼",
+};
+
+export const playerNameMap: { [key: string]: string } = {
+    "Erling Haaland": "엘링 홀란드",
+    "Harry Kane": "해리 케인",
+    "Ivan Toney": "아이반 토니",
+    "Marcus Rashford": "마커스 래시포드",
+    "Martinelli": "가브리엘 마르치넬리",
+    "Mohamed Salah": "모하메드 살라",
+    "Ollie Watkins": "올리 왓킨스",
+    "Bukayo Saka": "부카요 사카",
+    "Aleksandar Mitrović": "알렉산다르 미트로비치",
+    "Rodrigo": "로드리고",
+    "Callum Wilson": "칼럼 윌슨",
+    "Martin Ødegaard": "마르틴 외데고르"
+}
