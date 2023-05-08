869786fa44d07f571bcc370501fc4a9aba129198
woosang <woosang3078@hanmail.net>
2023-05-08 12:13:40 +0900

    style: 코드 포매팅

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
 app.get("/premierleague/scorers", async (req: Request, res: Response) => {
   try {
     const response = await axios.get(
@@ -45,22 +44,23 @@ app.get("/premierleague", async (req: Request, res: Response) => {
   }
 });
 
-app.get('/news', async (req: Request, res: Response) => {
+app.get("/news", async (req: Request, res: Response) => {
   try {
-    const response = await axios.get('https://openapi.naver.com/v1/search/news.json?query=%ED%94%84%EB%A6%AC%EB%AF%B8%EC%96%B4%EB%A6%AC%EA%B7%B8&display=10&start=1&sort=sim&startDate=20230101&endDate=20230508', 
-    {
-      headers: {
-        'X-Naver-Client-Id': "cAF0lQLJicO9hFcO8ZaG",
-        'X-Naver-Client-Secret': "nQ6CSn959Z"
+    const response = await axios.get(
+      "https://openapi.naver.com/v1/search/news.json?query=%ED%94%84%EB%A6%AC%EB%AF%B8%EC%96%B4%EB%A6%AC%EA%B7%B8&display=10&start=1&sort=sim&startDate=20230101&endDate=20230508",
+      {
+        headers: {
+          "X-Naver-Client-Id": "cAF0lQLJicO9hFcO8ZaG",
+          "X-Naver-Client-Secret": "nQ6CSn959Z",
+        },
       }
-    }
-    )
+    );
     res.send(response.data);
   } catch (error) {
-    console.error(error)
-    res.status(500).send("Interval Server Error")
+    console.error(error);
+    res.status(500).send("Interval Server Error");
   }
-})
+});
 
 const PORT = 4000;
 app.listen(PORT, () => {
