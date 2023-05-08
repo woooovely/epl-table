426e5ce73dfa8aae19aebdd24bbff11f3b9f130e
woosang <woosang3078@hanmail.net>
2023-05-08 11:23:35 +0900

    design: UI 수정

--- client/src/components/News/style.ts
+++ client/src/components/News/style.ts
@@ -9,15 +9,28 @@ export const Title = styled.p`
 export const NewsItemBlock = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 40px;
 `
 
 export const Thumbnail = styled.div`
   margin-right: 1rem;
+  margin-top: 50px;
+  border: 1px solid #E1E1E1;
+  display: flex;
+  width: 550px;
 `
 
 export const Image = styled.img`
   display: block;
-  width: 160px;
-  height: 100px;
+  width: 100px;
+  height: 70px;
+  padding: 10px;
   object-fit: cover;
+`
+
+export const NewsTitle = styled.p`
+  font-weight: 600;
+`
+
+export const Detail = styled.div`
 `
