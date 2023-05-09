e2f62af2fcbced0f98bd442c3383e603b9de1dfd
woosang <woosang3078@hanmail.net>
2023-05-09 20:19:10 +0900

    docs: 컴포넌트명 수정

--- client/src/pages/rank/premier/index.tsx
+++ client/src/pages/rank/premier/index.tsx
@@ -4,13 +4,13 @@ import Header from "../../../components/Header";
 import RankingLists from "../../../components/Ranking/premier";
 import SubHeader from '../../../components/SubHeader';
 
-const TeamRank = () => {
+const PremierRank = () => {
   return (
     <div>
       <Header />
-      <SubHeader title="팀 순위" />
+      <SubHeader title="프리미어리그 팀순위" />
       <S.BannerImage src={banner} alt="배너" />
-      <S.Text>England Premier League</S.Text>
+      <S.Text>잉글랜드 프리미어리그</S.Text>
       <S.Stick />
       <S.TableContainer>
         <RankingLists />
@@ -19,4 +19,4 @@ const TeamRank = () => {
   );
 };
 
-export default TeamRank;
+export default PremierRank;
