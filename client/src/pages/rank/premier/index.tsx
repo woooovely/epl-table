6a6c1080f712ad7b8de5db3a3f18664dc36dea65
woosang <woosang3078@hanmail.net>
2023-05-24 11:19:36 +0900

    docs: 페이지 탭 제목 할당

--- client/src/pages/rank/premier/index.tsx
+++ client/src/pages/rank/premier/index.tsx
@@ -3,8 +3,12 @@ import banner from "../../../assets/svgs/banner.svg";
 import Header from "../../../components/Header";
 import PLRankLists from "../../../components/Ranking/premier";
 import SubHeader from "../../../components/SubHeader";
+import useTitle from '../../../hook/title';
 
 const PremierRank = () => {
+  const titleUpdater = useTitle("불러오는 중...");
+  setTimeout(() => titleUpdater("프리미어리그 순위 - EPL Table"));
+
   return (
     <div>
       <Header />
