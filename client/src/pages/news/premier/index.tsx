ffb48bb23af4a3e9cb9fa3238008d64a760a159f
woosang <woosang3078@hanmail.net>
2023-05-24 11:11:06 +0900

    docs: 페이지 탭 제목 할당

--- client/src/pages/news/premier/index.tsx
+++ client/src/pages/news/premier/index.tsx
@@ -3,8 +3,12 @@ import * as S from "./style";
 import banner from "../../../assets/svgs/banner.svg";
 import NewsLists from "../../../components/News/premier/index";
 import SubHeader from "../../../components/SubHeader";
+import useTitle from '../../../hook/title';
 
 const PremierNews = () => {
+  const titleUpdater = useTitle('불러오는 중...')
+  setTimeout(() => titleUpdater('프리미어리그 뉴스 - EPL Table'))
+
   return (
     <S.Container>
       <Header />
