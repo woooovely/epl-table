06b2822c38f1125c8cb86d1f29348e6c7c0d73d3
woosang <woosang3078@hanmail.net>
2023-05-31 14:56:01 +0900

    docs: 클릭 이벤트 추가

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,27 +1,30 @@
-import * as S from './style';
+import * as S from "./style";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <>
-          <S.HeaderContainer>
-            <S.LogoText>EPL Table</S.LogoText>
-            <S.CategoryContainer>
-                <S.Categories>
-                  <S.LinkStyle to="/">홈</S.LinkStyle>
-                </S.Categories>
-                <S.Categories>
-                  <S.LinkStyle to="/pl/rank">팀 순위</S.LinkStyle>
-                </S.Categories>
-                <S.Categories>
-                  <S.LinkStyle to="/pl/scorer">득점왕 순위</S.LinkStyle>
-                </S.Categories>
-                <S.Categories>
-                  <S.LinkStyle to="/pl/news">해외축구 소식</S.LinkStyle>
-                </S.Categories>
-            </S.CategoryContainer>
-          </S.HeaderContainer>
-        </>
-    )
-}
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <S.HeaderContainer>
+        <S.LogoText onClick={() => navigate("/")}>EPL Table</S.LogoText>
+        <S.CategoryContainer>
+          <S.Categories>
+            <S.LinkStyle to="/">홈</S.LinkStyle>
+          </S.Categories>
+          <S.Categories>
+            <S.LinkStyle to="/pl/rank">팀 순위</S.LinkStyle>
+          </S.Categories>
+          <S.Categories>
+            <S.LinkStyle to="/pl/scorer">득점왕 순위</S.LinkStyle>
+          </S.Categories>
+          <S.Categories>
+            <S.LinkStyle to="/pl/news">해외축구 소식</S.LinkStyle>
+          </S.Categories>
+        </S.CategoryContainer>
+      </S.HeaderContainer>
+    </>
+  );
+};
 
 export default Header;
